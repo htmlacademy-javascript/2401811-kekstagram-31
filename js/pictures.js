ca5351813cce906cf2ddd14676c9ed05a085e124
sverch25 <zaichko@list.ru>
2024-03-25 22:19:03 +0500

    complete module7-task1

---
+++ js/pictures.js
@@ -0,0 +1,22 @@
+import { createPhotos } from './data';
+
+const pictureTemplate = document.querySelector('#picture').content;
+const picture = pictureTemplate.querySelector('.picture');
+const pictures = document.querySelector('.pictures');
+const createElement = createPhotos();
+const picturesFragment = document.createDocumentFragment();
+
+createElement.forEach((postedPicture) => {
+  const newPicture = picture.cloneNode(true);
+  const newPictureImage = newPicture.querySelector('.picture__img');
+  const newPictureComments = newPicture.querySelector('.picture__comments');
+  const newPictureLikes = newPicture.querySelector('.picture__likes');
+  newPictureImage.src = postedPicture.url;
+  newPictureImage.alt = postedPicture.description;
+  newPictureComments.textContent = postedPicture.comments.length;
+  newPictureLikes.textContent = postedPicture.likes;
+
+  picturesFragment.append(newPicture);
+});
+
+pictures.append(picturesFragment);
